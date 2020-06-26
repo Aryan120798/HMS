d1a5e8abd1eb832202b942dc4f06727040d18d3e
sagarsp123 <prabhusagar21@gmail.com>
2020-06-26 23:24:16 +0530

    Added issue med UI

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def home():
   return render_template('home.html')
 
+@app.route('/issuemed')
+def home3():
+  return render_template('issuemed.html')
+
+@app.route('/diagnostics')
+def home1():
+  return render_template('diagnostics.html')
+
+@app.route('/finalbill')
+def home2():
+  return render_template('finalbill.html')
+
 @app.errorhandler(404)
 def _404Page(str):
   return render_template('404.html')
